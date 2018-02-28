c86bc142718dd3f45230eb0d7d6b8bb3a05e61e5
Bruna Sayuri <bruna_susuke@hotmail.com>
2018-02-27 22:18:16 -0300

    controle transacional

--- mavenproject1/agenda/src/main/java/br/senac/tads/pi3b/agenda/Agenda.java
+++ mavenproject1/agenda/src/main/java/br/senac/tads/pi3b/agenda/Agenda.java
@@ -1,5 +1,6 @@
 package br.senac.tads.pi3b.agenda;
 
+import com.mysql.jdbc.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -59,22 +60,60 @@ public class Agenda {
     }
 
     public void incluir() throws ClassNotFoundException, SQLException {
-        try (Connection conn = obterConexao();
-                PreparedStatement stmt = conn.prepareStatement("INSERT INTO agenda.PESSOA (nome, dtnasc) VALUES (?, ?)")) {
-            stmt.setString(1, "Jonas Ribeiro");
-            GregorianCalendar cal = new GregorianCalendar(1995, 10, 11);
-            stmt.setDate(2, new java.sql.Date(cal.getTimeInMillis()));
 
-            int status = stmt.executeUpdate();
+        try (Connection conn = obterConexao()) {
+            conn.setAutoCommit(false);
 
-            System.out.println("status: " + status);
+            try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO agenda.PESSOA (nome, dtnasc) VALUES (?, ?)",
+                    Statement.RETURN_GENERATED_KEYS)) {
+                stmt.setString(1, "Jonas Ribeiro");
+                GregorianCalendar cal = new GregorianCalendar(1995, 10, 11);
+                stmt.setDate(2, new java.sql.Date(cal.getTimeInMillis()));
+
+                int status = stmt.executeUpdate();
+
+                System.out.println("status: " + status);
+
+                ResultSet generatedKeys = stmt.getGeneratedKeys();
+
+                if (generatedKeys.next()) {
+                    long idPessoa = generatedKeys.getLong(1);
+
+                    try (PreparedStatement stmt2 = conn.prepareStatement("INSERT INTO agenda.CONTATO (tipo, valor, idpessoa) VALUES (?, ?, ?)")) {
+
+                        //email
+                        stmt2.setInt(1, 1);
+                        stmt2.setString(2, "alou@teste.com");
+                        stmt2.setLong(3, idPessoa);
+
+                        stmt2.executeUpdate();
+
+                    }
+                    try (PreparedStatement stmt3 = conn.prepareStatement("INSERT INTO agenda.CONTATO (tipo, valor, idpessoa) VALUES (?, ?, ?)")) {
+
+                        //telefone
+                        stmt3.setInt(1, 2);
+                        stmt3.setString(2, "(11) 5555-5555");
+                        stmt3.setLong(3, idPessoa);
+
+                        stmt3.executeUpdate();
+
+                    }
+
+                    conn.commit();
+                }
+            } catch (SQLException e) {
+                //em caso de erro, volta para a comparação inicial
+                conn.rollback();
+                throw e;
+            }
         }
     }
 
     public static void main(String[] args) {
         Agenda agenda = new Agenda();
         try {
-            //agenda.incluir();
+            agenda.incluir();
 
             List<Pessoa> lista = agenda.listar();
 
